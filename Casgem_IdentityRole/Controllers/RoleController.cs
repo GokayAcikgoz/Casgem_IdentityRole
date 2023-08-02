209909b7396adc82ec0202d791744624bfc36ee2
gokay <gokayacikgoz1@gmail.com>
2023-08-02 14:12:07 +0300

    identity role

--- Casgem_IdentityRole/Controllers/RoleController.cs
+++ Casgem_IdentityRole/Controllers/RoleController.cs
@@ -2,6 +2,7 @@
 using Casgem_IdentityRole.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Casgem_IdentityRole.Controllers
 {
@@ -46,7 +47,54 @@ namespace Casgem_IdentityRole.Controllers
             {
                 return RedirectToAction("RoleList");
             }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var value = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == id);
+            await _roleManager.DeleteAsync(value);
             return RedirectToAction("RoleList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateRole(int id)
+        {
+            var value = await _roleManager.Roles.FirstOrDefaultAsync(x => x.Id == id);
+            UpdateRoleViewModel model = new UpdateRoleViewModel()
+            {
+                Id = value.Id,
+                Name = value.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(UpdateRoleViewModel model)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == model.Id);
+            value.Name = model.Name;
+            await _roleManager.UpdateAsync(value);
+            return RedirectToAction("Rolelist");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(int id)
+        {
+            var user = await _userManager.Users.FirstOrDefaultAsync(x => x.Id == id);
+            var roles = _roleManager.Roles.ToList();
+            var userRoles= await _userManager.GetRolesAsync(user);
+            List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+            foreach (var item in roles)
+            {
+                RoleAssignViewModel model = new RoleAssignViewModel();
+                model.RoleID = item.Id;
+                model.RoleName = item.Name;
+                model.RoleExist = userRoles.Contains(item.Name);
+                roleAssignViewModels.Add(model);
+            }
+            return View(roleAssignViewModels);
         }
     }
 }
